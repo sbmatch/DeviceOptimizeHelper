34e73a310458f7143916e6c76907b65151a7d623
root <maping@mpcloud.top>
2023-09-15 00:34:01 +0800

    modified:   .idea/misc.xml 	modified:   app/build.gradle 	modified:   app/src/main/java/ma/DeviceOptimizeHelper/BaseApplication/BaseApplication.java

--- app/src/main/java/ma/DeviceOptimizeHelper/BaseApplication/BaseApplication.java
+++ app/src/main/java/ma/DeviceOptimizeHelper/BaseApplication/BaseApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.rosan.dhizuku.api.Dhizuku;
 
 import org.lsposed.hiddenapibypass.HiddenApiBypass;
@@ -16,5 +19,24 @@ public class BaseApplication extends Application {
             HiddenApiBypass.addHiddenApiExemptions("");
         }
         Dhizuku.init(base);
+
+        // 设置全局异常处理器
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> handleCrash(e));
+
     }
+
+    private void handleCrash(Throwable throwable) {
+        // 在这里你可以根据自己的需求进行处理，例如显示对话框或者保存堆栈信息到文件等
+        String stackTrace = getStackTrace(throwable);
+        new MaterialAlertDialogBuilder(getApplicationContext()).setTitle("崩溃了🤣👉🤡").setMessage(stackTrace).setPositiveButton("Ok", null).create().show();
+    }
+
+    private String getStackTrace(Throwable throwable) {
+        // 将 Throwable 对象的堆栈信息转换为字符串形式
+        java.io.StringWriter sw = new java.io.StringWriter();
+        java.io.PrintWriter pw = new java.io.PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
+    }
+
 }
