e4aafd22307b10350d5e9c02a5e244332ee66a29
admin <3207754367@qq.com>
2024-08-04 17:48:33 +0800

    重构系统服务管理类

--- app/src/main/java/ma/DeviceOptimizeHelper/Utils/ServiceManager.java
+++ app/src/main/java/ma/DeviceOptimizeHelper/Utils/ServiceManager.java
@@ -2,19 +2,71 @@ package ma.DeviceOptimizeHelper.Utils;
 
 import android.annotation.SuppressLint;
 import android.os.IBinder;
+import android.os.IInterface;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ServiceManager {
 
-    public static IBinder getSystemService(String name){
+    private ServiceManager() {
+        /* not instantiable */
+    }
+    private static IInterface getServiceInterface(String service, String type){
         try {
-            @SuppressLint({"PrivateApi", "DiscouragedPrivateApi"})
+            @SuppressLint({"DiscouragedPrivateApi", "PrivateApi"})
             Method getServiceMethod = Class.forName("android.os.ServiceManager").getDeclaredMethod("getService", String.class);
-            getServiceMethod.setAccessible(true);
-            return (IBinder) getServiceMethod.invoke(null, name);
+            IBinder binder = (IBinder) getServiceMethod.invoke(null, service);
+            Method asInterfaceMethod = Class.forName(type + "$Stub").getMethod("asInterface", IBinder.class);
+            return (IInterface) asInterfaceMethod.invoke(null, binder);
         } catch (NullPointerException | IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static IBinder getService(String service){
+        try {
+            @SuppressLint({"DiscouragedPrivateApi", "PrivateApi"})
+            Method getServiceMethod = Class.forName("android.os.ServiceManager").getMethod("getService", String.class);
+            return  (IBinder) getServiceMethod.invoke(null, service);
+        } catch (NullPointerException | IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static IBinder checkService(String name){
+        try {
+            @SuppressLint({"DiscouragedPrivateApi", "PrivateApi"})
+            Method getServiceMethod = Class.forName("android.os.ServiceManager").getMethod("checkService", String.class);
+            return  (IBinder) getServiceMethod.invoke(null, name);
+        } catch (NullPointerException | IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @SuppressLint("PrivateApi")
+    public static String[] listServices() {
+        try {
+            return (String[]) Class.forName("android.os.ServiceManager").getMethod("listServices").invoke(null);
+        } catch (NullPointerException | IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public final static ActivityManager getActivityManager = new ActivityManager(getServiceInterface("activity", "android.app.IActivityManager"));
+    public final static UserManager getUserManager = new UserManager(getServiceInterface("user","android.os.IUserManager"));
+    public final static PackageManager getPackageManager = new PackageManager(getServiceInterface("package", "android.content.pm.IPackageManager"));
+
+    //public final static NotificationManager getNotificationManager = new NotificationManager(getService("notification","android.app.INotificationManager"));
+
+
+
+    public static Object getAppRunningControlManager;
+
+    static {
+        try {
+            getAppRunningControlManager = ReflectUtil.callStaticObjectMethod(Class.forName("miui.security.AppRunningControlManager"), "getInstance");
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
