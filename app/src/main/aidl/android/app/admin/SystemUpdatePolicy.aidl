34bf4e6ac8703757d561faef8448d7d7c69760fe
admin <3207754367@qq.com>
2024-08-07 12:16:36 +0800

    移除此接口类

--- app/src/main/aidl/android/app/admin/SystemUpdatePolicy.aidl
+++
@@ -1,3 +0,0 @@
-package android.app.admin;
-
-parcelable SystemUpdatePolicy;
---
+++ app/src/main/java/ma/DeviceOptimizeHelper/Utils/AppRunningControlManager.java
@@ -0,0 +1,10 @@
+package ma.DeviceOptimizeHelper.Utils;
+
+public class AppRunningControlManager {
+    Object appRunningControlManager;
+    public AppRunningControlManager(Object manager){
+        appRunningControlManager = manager;
+    }
+
+
+}
