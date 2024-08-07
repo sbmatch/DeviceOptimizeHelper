07da95af5757f68abe4f80eac93e42cecc257257
admin <3207754367@qq.com>
2024-08-08 07:54:19 +0800

    新增应用运行控制器，此为MIUI / Hyper 系统Framework层实现

--- app/src/main/java/ma/DeviceOptimizeHelper/Utils/AppRunningControlManager.java
+++ app/src/main/java/ma/DeviceOptimizeHelper/Utils/AppRunningControlManager.java
@@ -1,8 +1,24 @@
 package ma.DeviceOptimizeHelper.Utils;
 
+import android.content.Intent;
+
+import java.util.List;
+
 public class AppRunningControlManager {
-    Object appRunningControlManager;
+    private Object appRunningControlManager;
     public AppRunningControlManager(Object manager){
         appRunningControlManager = manager;
+    }
+
+    public void setBlackListEnable(boolean isEnable){
+        ReflectUtil.callObjectMethod2(appRunningControlManager, "setBlackListEnable", isEnable);
+    }
+
+    public void setDisallowRunningList(List<String> list, Intent intent){
+        ReflectUtil.callObjectMethod2(appRunningControlManager, "setDisallowRunningList", list, intent);
+    }
+
+    public List<String> getNotDisallowList(){
+        return (List<String>) ReflectUtil.callObjectMethod2(appRunningControlManager, "getNotDisallowList");
     }
 }
