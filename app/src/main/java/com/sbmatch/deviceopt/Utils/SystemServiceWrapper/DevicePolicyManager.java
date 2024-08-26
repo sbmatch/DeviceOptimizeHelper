0aa8c5a265d2f19836a79798032e7a8daa508e0f
admin <3207754367@qq.com>
2024-08-26 12:39:24 +0800

    adminComponent改从dpm直接获取, 可用于未来接入其他dpm管理器应用

--- app/src/main/java/com/sbmatch/deviceopt/Utils/SystemServiceWrapper/DevicePolicyManager.java
+++ app/src/main/java/com/sbmatch/deviceopt/Utils/SystemServiceWrapper/DevicePolicyManager.java
@@ -8,7 +8,6 @@ import android.os.IInterface;
 import com.rosan.dhizuku.api.Dhizuku;
 import com.sbmatch.deviceopt.Utils.ContextUtil;
 import com.sbmatch.deviceopt.Utils.ReflectUtil;
-import com.sbmatch.deviceopt.Utils.UserHandle;
 
 import java.util.Collection;
 
@@ -25,17 +24,18 @@ public class DevicePolicyManager {
     private DevicePolicyManager(Context context){
         this.dpm = (android.app.admin.DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
     }
+    public static DevicePolicyManager get(Context context){
+        if (devicePolicyManager == null) {
+            devicePolicyManager = new DevicePolicyManager(context);
+            if (adminComponent == null) adminComponent = devicePolicyManager.getDeviceOwnerComponentOnUser();
+        }
+        return devicePolicyManager;
+    }
     public static DevicePolicyManager get(){
         if (devicePolicyManager == null) devicePolicyManager = new DevicePolicyManager();
         return devicePolicyManager;
     }
-    public static DevicePolicyManager get(Context context){
-        if (devicePolicyManager == null) {
-            devicePolicyManager = new DevicePolicyManager(context);
-            if (adminComponent == null) adminComponent = Dhizuku.getOwnerComponent();
-        }
-        return devicePolicyManager;
-    }
+
     private DevicePolicyManager(){
         this.dpm = (android.app.admin.DevicePolicyManager) ContextUtil.currentApplication().getSystemService(Context.DEVICE_POLICY_SERVICE);
     }
@@ -47,25 +47,25 @@ public class DevicePolicyManager {
     public void setOrganizationName(String name) {
         if (manager != null) ReflectUtil.callObjectMethod2(manager,
                 "setOrganizationName",
-                Dhizuku.getOwnerComponent(),
-                Dhizuku.getOwnerPackageName(),
+                adminComponent,
+                adminComponent.getPackageName(),
                 name);
         if (dpm != null) dpm.setOrganizationName(adminComponent, name);
     }
 
     public void addUserRestriction(String key) {
-        if (manager != null) ReflectUtil.callObjectMethod2(manager, "setUserRestriction", Dhizuku.getOwnerComponent(), Dhizuku.getOwnerPackageName(), key, true, true);
+        if (manager != null) ReflectUtil.callObjectMethod2(manager, "setUserRestriction", adminComponent, adminComponent.getPackageName(), key, true, true);
         if (dpm != null) dpm.addUserRestriction(adminComponent, key);
     }
 
     public void clearUserRestriction(String key) {
-        if (manager != null) ReflectUtil.callObjectMethod2(manager, "setUserRestriction", Dhizuku.getOwnerComponent(), Dhizuku.getOwnerPackageName(), key, false, true);
+        if (manager != null) ReflectUtil.callObjectMethod2(manager, "setUserRestriction", adminComponent, adminComponent.getPackageName(), key, false, true);
         if (dpm != null) dpm.clearUserRestriction(adminComponent, key);
     }
 
     public void setUninstallBlocked(String packageName, boolean blockUninstall) {
         if (dpm != null) dpm.setUninstallBlocked(adminComponent, packageName, blockUninstall);
-        if (manager != null) ReflectUtil.callObjectMethod2(manager, "setUninstallBlocked", Dhizuku.getOwnerComponent(), Dhizuku.getOwnerPackageName(), packageName, blockUninstall);
+        if (manager != null) ReflectUtil.callObjectMethod2(manager, "setUninstallBlocked", adminComponent, adminComponent.getPackageName(), packageName, blockUninstall);
     }
 
     public boolean isUninstallBlocked(String packageName) {
@@ -75,17 +75,19 @@ public class DevicePolicyManager {
     }
     public String[] setPackagesSuspended(String[] packageNames, boolean suspended, SuspendDialogInfo dialogInfo) {
         if (dpm != null) return dpm.setPackagesSuspended(adminComponent, packageNames, suspended);
-        if (manager != null) return (String[]) ReflectUtil.callObjectMethod2(manager, "setPackagesSuspended", Dhizuku.getOwnerComponent(), Dhizuku.getOwnerPackageName(), packageNames, suspended);
+        if (manager != null) return (String[]) ReflectUtil.callObjectMethod2(manager, "setPackagesSuspended", adminComponent, adminComponent.getPackageName(), packageNames, suspended);
         return new String[0];
     }
     
     public boolean isPackageSuspended(String packageName) {
         if (dpm != null) return (boolean) ReflectUtil.callObjectMethod2(dpm,"isPackageSuspended", adminComponent, packageName);
-        if (manager != null) return (boolean) ReflectUtil.callObjectMethod2(manager,"isPackageSuspended", Dhizuku.getOwnerComponent(), Dhizuku.getOwnerPackageName(), packageName);
+        if (manager != null) return (boolean) ReflectUtil.callObjectMethod2(manager,"isPackageSuspended", adminComponent, adminComponent.getPackageName(), packageName);
         return false;
     }
 
     public ComponentName getDeviceOwnerComponentOnUser(){
-        return (ComponentName) ReflectUtil.callObjectMethod2(manager, "getDeviceOwnerComponentOnUser", UserHandle.myUserId());
+        if (dpm != null) return (ComponentName) ReflectUtil.callObjectMethod2(dpm, "getDeviceOwnerComponentOnCallingUser");
+        if (manager != null) return (ComponentName) ReflectUtil.callObjectMethod2(manager, "getDeviceOwnerComponent", true);
+        return adminComponent;
     }
 }
