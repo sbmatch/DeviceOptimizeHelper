409a8669ac29532934f250707de38d43a63bdef6
admin <3207754367@qq.com>
2024-08-26 12:30:18 +0800

    修改调用方式为直接反射android.os.UserManager方法

--- app/src/main/java/com/sbmatch/deviceopt/Utils/SystemServiceWrapper/UserManager.java
+++ app/src/main/java/com/sbmatch/deviceopt/Utils/SystemServiceWrapper/UserManager.java
@@ -1,29 +1,44 @@
 package com.sbmatch.deviceopt.Utils.SystemServiceWrapper;
 
 
-import android.os.IInterface;
+import android.content.Context;
 
+import com.sbmatch.deviceopt.Utils.ContextUtil;
 import com.sbmatch.deviceopt.Utils.ReflectUtil;
-import com.sbmatch.deviceopt.Utils.UserHandle;
 
 import java.util.Collection;
 
 public class UserManager {
-    private IInterface manager;
-
-    public UserManager(IInterface manager){
-        this.manager = manager;
+    private final android.os.UserManager um;
+    private static UserManager userManager;
+    private UserManager(Context context){
+        this.um = (android.os.UserManager) context.getSystemService(Context.USER_SERVICE);
     }
 
-    public Collection<?> getALLUserRestrictionsWithReflect(){
+    public static UserManager get(Context context) {
+        if (userManager == null) userManager = new UserManager(context);
+        return userManager;
+    }
+
+    private UserManager(){
+        this.um = (android.os.UserManager) ContextUtil.getContext().getSystemService(Context.USER_SERVICE);
+    }
+
+    public static UserManager get() {
+        if (userManager == null) userManager = new UserManager();
+        return userManager;
+    }
+
+
+    public static Collection<?> getALLUserRestrictionsWithReflect(){
        return ReflectUtil.getFieldsByPrefixMatch(android.os.UserManager.class, "DISALLOW_").values();
     }
 
     public boolean hasUserRestriction(String restrictionKey){
-        return (boolean) ReflectUtil.callObjectMethod2(manager, "hasUserRestriction", restrictionKey, UserHandle.myUserId());
+        return (boolean) ReflectUtil.callObjectMethod2(um, "hasUserRestriction", restrictionKey);
     }
 
     public void setUserRestriction(String key, boolean value){
-        ReflectUtil.callObjectMethod2(manager, "setUserRestriction", key, value, UserHandle.myUserId());
+        ReflectUtil.callObjectMethod2(um, "setUserRestriction", key, value);
     }
 }
